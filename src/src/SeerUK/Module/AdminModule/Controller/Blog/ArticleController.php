1eb4d0480d94877489938a2e13e8d42a76c8a23e
Elliot Wright <wright.elliot@gmail.com>
2014-08-30 19:28:52 +0100

    Style tweaks for Article overview. Lots of font size changes.

--- src/src/SeerUK/Module/AdminModule/Controller/Blog/ArticleController.php
+++ src/src/SeerUK/Module/AdminModule/Controller/Blog/ArticleController.php
@@ -11,6 +11,7 @@
 
 namespace SeerUK\Module\AdminModule\Controller\Blog;
 
+use Aegis\Aegis;
 use SeerUK\Module\BlogModule\Data\Entity\Article;
 use SeerUK\Module\BlogModule\Form\Type\ArticleType;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,26 +27,57 @@ use Trident\Module\FrameworkModule\Controller\Controller;
 class ArticleController extends Controller
 {
     /**
+     * @var Aegis
+     */
+    protected $security;
+
+    /**
+     * Constructor.
+     *
+     * @param Aegis $security
+     */
+    public function __construct(Aegis $security)
+    {
+        $this->security = $security;
+
+        if ( ! $this->security->isGranted('ROLE_ADMIN')) {
+            throw new ForbiddenHttpException('You are not granted access to this area.');
+        }
+    }
+
+    /**
+     * Blog articles overview
+     *
+     * @return Response
+     */
+    public function overviewAction()
+    {
+        $ar = $this->get('bm.repository.article');
+
+        $articles = $ar->findAll();
+
+        return $this->render('SeerUKAdminModule:Blog/Article:overview.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
      * Edit a blog article
      *
      * @param integer $id
      *
      * @return Response
      */
-    public function editAction($id)
+    public function editAction($slug)
     {
-        if ( ! $this->get('security')->isGranted('ROLE_ADMIN')) {
-            throw new ForbiddenHttpException('You are not granted access to this area.');
-        }
-
         $ar = $this->get('bm.repository.article');
         $em = $this->get('doctrine.orm.entity_manager');
         $ff = $this->get('form.factory');
 
-        $article = $ar->findOneById($id);
+        $article = $ar->findOneBySlug($slug);
 
         if ( ! $article) {
-            throw new NotFoundHttpException(sprintf(Article::ERR_NO_ARTICLE, $id));
+            throw new NotFoundHttpException(sprintf(Article::ERR_NO_ARTICLE, $slug));
         }
 
         $form = $ff->create(new ArticleType, $article);
@@ -58,10 +90,10 @@ class ArticleController extends Controller
             $em->persist($article);
             $em->flush();
 
-            $caching->remove("rendered.blog.article.$id");
+            $caching->remove("rendered.blog.article.$slug");
 
             // return $this->redirect($this->generateUrl('bm_blog_view', [
-            //     'id' => $article->getId()
+            //     'slug' => $article->getSlug()
             // ]));
         }
 
--- src/src/SeerUK/Module/AdminModule/module/config/services.php
+++ src/src/SeerUK/Module/AdminModule/module/config/services.php
@@ -1,6 +1,10 @@
 <?php
 
+use SeerUK\Module\AdminModule\Controller\Blog\ArticleController;
+
 return function($container) {
     // Services
-
+    $container->set('am.controller.article', function($c) {
+        return new ArticleController($c->get('security'));
+    });
 };
